ce246e41f3d383442d9ca99992977933b71196fb
Samantha Smith <37231841+SamanthaKaye@users.noreply.github.com>
2019-08-12 10:58:21 -0500

    Creating, Adding, and Removing $

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		BankAccount myBankAccount = new BankAccount ( 4141,  100);
-		System.out.println(" The current account balance is $" + myBankAccount.account_balance);
+		System.out.println(" The current account balance is $" + myBankAccount.account_balance );
 		myBankAccount.deposit( -60);
 		myBankAccount.withdraw(  150);
 		myBankAccount.deposit(  100);
